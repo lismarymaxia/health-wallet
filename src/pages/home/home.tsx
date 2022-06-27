f80ab0369ff5da9d63964b7a2c11cc2df55d5661
christopher-1988 <christopher_carnevale@outlook.com>
2022-06-27 11:41:41 -0500

    cambios de slider

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import {
-  IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, 
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
   IonContent,
   IonPage,
   IonIcon,
@@ -12,6 +16,8 @@ import {
   IonLabel,
   IonList,
   useIonViewDidEnter,
+  IonSlide,
+  IonSlides,
 } from "@ionic/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -32,6 +38,12 @@ import "./home.css";
 
 const Home: React.FC = () => {
   const cedula = useSelector((state: any) => state.reducerAuth.user.cedula);
+  const slideOpts = {
+    initialSlide: 2,
+    speed: 400,
+    slidesPerView: 2,
+    spaceBetween: 30,
+  };
   const [load, setLoad] = useState<Boolean>(true);
   const [data, setData] = useState<any>({
     cedula: "",
@@ -75,92 +87,145 @@ const Home: React.FC = () => {
   return (
     <IonPage className="fondo">
       {/*<Header title="Health Wallet" isbotton={false} isBuger={true} />*/}
-      
+
       <IonContent fullscreen>
         <IonGrid className="bg-light">
-        <IonRow className="bg-info-alt pt-3 pb-2 text-white">
-          <IonCol size="8" className="px-3">
-            Lunes, 13 de junio
-          </IonCol>
-          <IonCol size="4" className="pr-4 fs-20 text-right">
-            <FontAwesomeIcon icon={faBell} className="mr-3" />
-            <FontAwesomeIcon icon={faShareNodes} className="mr-0" />
-          </IonCol>
-          <IonCol size="12" className="px-3">
-            <h3 className="text-white">¡Hola Usuario!</h3>
-          </IonCol>
-        </IonRow>
+          <IonRow className="bg-info-alt pt-3 pb-2 text-white">
+            <IonCol size="8" className="px-3">
+              Lunes, 13 de junio
+            </IonCol>
+            <IonCol size="4" className="pr-4 fs-20 text-right">
+              <FontAwesomeIcon icon={faBell} className="mr-3" />
+              <FontAwesomeIcon icon={faShareNodes} className="mr-0" />
+            </IonCol>
+            <IonCol size="12" className="px-3">
+              <h3 className="text-white">¡Hola Usuario!</h3>
+            </IonCol>
+          </IonRow>
 
-        <IonRow className="bg-info-alt">
-          <IonCol size="12" className="px-3 mb-2">
-            <IonCard className="m-0">
-              <IonCardContent>
-                <IonImg src={'./images/auxiliar-enfermeria.png'} />
-              </IonCardContent>
-            </IonCard>
-          </IonCol>
-        </IonRow>
+          <IonRow className="bg-info-alt">
+            <IonCol size="12" className="px-3 mb-2">
+              <IonCard className="m-0">
+                <IonCardContent>
+                  <IonImg src={"./images/auxiliar-enfermeria.png"} />
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
 
-        <IonRow className="mt-4">
-          <IonCol size="12" className="px-3">
-            <h5 className="font-w600 fs-16 text-dark">Próximos turnos</h5>
-            
-            <IonCard className="m-0" style={{width: "12rem", height:"auto"}}>
-              <IonCardContent>
-                <div className="fs-24 font-w800 d-inline text-info">15 Jun</div>
-                <div className="fs-14 float-right d-inline bg-light py-1 px-2 rounded-sm">17:30</div>
-                <div>
-                  <p className="mb-0 fs-16 text-dark">Traumatología</p>
-                  <p className="mb-0 text-dark">Dr. Juan Fernando</p>
-                </div>
-              </IonCardContent>
-            </IonCard>
+          <IonRow className="mt-4">
+            <IonCol size="12" className="px-3">
+              <h5 className="font-w600 fs-16 text-dark">Próximos turnos</h5>
+              <IonSlides pager={false} options={slideOpts}>
+                <IonSlide>
+                  <IonCard
+                    className="m-0"
+                    style={{ width: "12rem", height: "auto" }}
+                  >
+                    <IonCardContent>
+                      <div className="fs-24 font-w800 d-inline text-info">
+                        15 Jun
+                      </div>
+                      <div className="fs-14 float-right d-inline bg-light py-1 px-2 rounded-sm">
+                        17:30
+                      </div>
+                      <div>
+                        <p className="mb-0 fs-16 text-dark">Traumatología</p>
+                        <p className="mb-0 text-dark">Dr. Juan Fernando</p>
+                      </div>
+                    </IonCardContent>
+                  </IonCard>{" "}
+                </IonSlide>
+                <IonSlide>
+                  <IonCard
+                    className="m-0"
+                    style={{ width: "12rem", height: "auto" }}
+                  >
+                    <IonCardContent>
+                      <div className="fs-24 font-w800 d-inline text-info">
+                        20 Jun
+                      </div>
+                      <div className="fs-14 float-right d-inline bg-light py-1 px-2 rounded-sm">
+                        09:30
+                      </div>
+                      <div>
+                        <p className="mb-0 fs-16 text-dark">Odontología</p>
+                        <p className="mb-0 text-dark">Dra. Maria Camila</p>
+                      </div>
+                    </IonCardContent>
+                  </IonCard>{" "}
+                </IonSlide>
+                <IonSlide>
+                  <IonCard
+                    className="m-0"
+                    style={{ width: "12rem", height: "auto" }}
+                  >
+                    <IonCardContent>
+                      <div className="fs-24 font-w800 d-inline text-info">
+                        25 Jun
+                      </div>
+                      <div className="fs-14 float-right d-inline bg-light py-1 px-2 rounded-sm">
+                        17:30
+                      </div>
+                      <div>
+                        <p className="mb-0 fs-16 text-dark">Traumatología</p>
+                        <p className="mb-0 text-dark">Dr. Juan Fernando</p>
+                      </div>
+                    </IonCardContent>
+                  </IonCard>{" "}
+                </IonSlide>
+              </IonSlides>
+            </IonCol>
+          </IonRow>
 
-            <IonCard className="m-0" style={{width: "12rem", height:"auto"}}>
-              <IonCardContent>
-                <div className="fs-24 font-w800 d-inline text-info">20 Jun</div>
-                <div className="fs-14 float-right d-inline bg-light py-1 px-2 rounded-sm">09:30</div>
-                <div>
-                  <p className="mb-0 fs-16 text-dark">Odontología</p>
-                  <p className="mb-0 text-dark">Dra. Maria Camila</p>
-                </div>
-              </IonCardContent>
-            </IonCard>
-          </IonCol>
-        </IonRow>
+          <IonRow className="mt-4">
+            <IonCol size="12" className="px-3">
+              <h5 className="font-w600 fs-16 text-dark">
+                Mis afiliados favoritos
+              </h5>
 
-        <IonRow className="mt-4">
-          <IonCol size="12" className="px-3">
-            <h5 className="font-w600 fs-16 text-dark">Mis afiliados favoritos</h5>
-
-            <IonCard className="m-0" style={{height:"auto"}}>
-              <IonCardContent>
-                <div className="border-bottom pt-3 pb-4">
-                  <IonImg src={'./images/osdl.png'} style={{width:"60px"}} className="float-left mr-2"/>
-                  <div className="fs-16 text-info">
-                    Centro médico OSDE
-                    <FontAwesomeIcon icon={faHeart} className="mr-0 float-right" />
+              <IonCard className="m-0" style={{ height: "auto" }}>
+                <IonCardContent>
+                  <div className="border-bottom pt-3 pb-4">
+                    <IonImg
+                      src={"./images/osdl.png"}
+                      style={{ width: "60px" }}
+                      className="float-left mr-2"
+                    />
+                    <div className="fs-16 text-info">
+                      Centro médico OSDE
+                      <FontAwesomeIcon
+                        icon={faHeart}
+                        className="mr-0 float-right"
+                      />
+                    </div>
+                    <div>
+                      <p className="mb-0">Cll 13a #76-52 - Piso 1</p>
+                    </div>
                   </div>
-                  <div>
-                    <p className="mb-0">Cll 13a #76-52 - Piso 1</p>
-                  </div>
-                </div>
 
-                <div className="pt-3 pb-4">
-                  <IonImg src={'./images/osdl.png'} style={{width:"60px"}} className="float-left mr-2"/>
-                  <div className="fs-16 text-info">
-                    Centro médico OSDE
-                    <FontAwesomeIcon icon={faHeart} className="mr-0 float-right" />
+                  <div className="pt-3 pb-4">
+                    <IonImg
+                      src={"./images/osdl.png"}
+                      style={{ width: "60px" }}
+                      className="float-left mr-2"
+                    />
+                    <div className="fs-16 text-info">
+                      Centro médico OSDE
+                      <FontAwesomeIcon
+                        icon={faHeart}
+                        className="mr-0 float-right"
+                      />
+                    </div>
+                    <div>
+                      <p className="mb-0">Cll 13a #76-52 - Piso 1</p>
+                    </div>
                   </div>
-                  <div>
-                    <p className="mb-0">Cll 13a #76-52 - Piso 1</p>
-                  </div>
-                </div>
-              </IonCardContent>
-            </IonCard>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
 
         <IonList
           className="acordion__fondo"
