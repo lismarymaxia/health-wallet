b0fe4d3350f8c24f7e4b4bf772e5929df9527e26
christopher-1988 <christopher_carnevale@outlook.com>
2022-07-11 12:31:37 -0500

    porgramacion modificacion del compoenete afiliado se agrego la carga dinamica

--- src/pages/afiliados/afiliados.tsx
+++ src/pages/afiliados/afiliados.tsx
@@ -1,45 +1,64 @@
 import {
-  IonButton,
   IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
   IonPage,
   IonSearchbar,
-  IonToolbar,
-  IonTitle,
-  IonGrid,  
-  IonProgressBar,
+  IonGrid,
   IonRow,
   IonCol,
   IonCard,
   IonCardContent,
-  IonImg
+  IonImg,
+  IonProgressBar,
 } from "@ionic/react";
 import { Header } from "../../components";
-import { arrowBackOutline } from "ionicons/icons";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { afiliados } from "../../data";
 import { Link } from "react-router-dom";
 import "../../style/tema.css";
 import "./afiliados.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faSliders } from "@fortawesome/free-solid-svg-icons";
+import { servicesWh } from "../../servicios/servicios";
 
 const Afiliados = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState<any>([]);
   const [load, setLoad] = useState<Boolean>(true);
 
   const pacsearch = useCallback((searc: any) => {
     return function name(params: any) {
-      return params.name.toUpperCase().includes(searc.toUpperCase());
+      return params.nombre.toUpperCase().includes(searc.toUpperCase());
     };
   }, []);
 
+  useEffect(() => {
+    setLoad(true);
+    servicesWh
+      .get("/controller/afiliados.php", {
+        params: {
+          op: "getAfiliados",
+          imestamp: new Date().getTime(),
+        },
+        responseType: "json",
+      })
+      .then((rsp) => {
+        const { data, status } = rsp;
+        if (status === 200) {
+          if (data.data) {
+            setLoad(false);
+            setData(data.data);
+          } else {
+            setLoad(false);
+            setData([]);
+          }
+        }
+      })
+      .catch((e) => {
+        console.warn(e);
+      });
+  }, []);
 
-  {/*if (load) {
+  if (load) {
     return (
       <IonPage className="fondo">
         <Header title="Afiliados" isbotton={true} isBuger={false} />
@@ -48,7 +67,7 @@ const Afiliados = () => {
         </IonContent>
       </IonPage>
     );
-  }*/}
+  }
 
   return (
     <IonPage className="fondo">
@@ -89,34 +108,48 @@ const Afiliados = () => {
 
           <IonRow>
             <IonCol size="12" className="px-3">
-              <h5 className="font-w600 fs-16 text-blue-dark">
-                Afiliados
-              </h5>
-            
-              {afiliados.filter(pacsearch(searchTerm)).map((movie: any) => {
-                return (
-                  <IonCard className="m-0 mb-2 pb-2 card-slide w-100 afiliados">
-                    <IonCardContent className="card-content-slide" id={`employeeItem_${movie.id}`} key={movie.id}>
-                      <IonImg
-                        src={movie.image}
-                        alt="employee avatar"
-                        style={{ width: "50px" }}
-                        className="float-left mr-2"
-                      />
-                      <div className="fs-15 font-w600 text-info mt-1 title">
-                        <span className="w-100">{movie.name}</span>
-                        <span><FontAwesomeIcon icon={faHeart} className="mr-0 float-right" /></span>
-                      </div>
-                      <div>
-                        <p className="mb-0 mt-1 fs-12 d-flex">
-                          <span>Cll 13a #76-52 - Piso 1</span>
-                        </p>
-                      </div>
-                    </IonCardContent>
-                  </IonCard>
-                );
-              })}
-              </IonCol>          
+              <h5 className="font-w600 fs-16 text-blue-dark">Afiliados</h5>
+
+              {data.length > 0
+                ? data.filter(pacsearch(searchTerm)).map((item: any) => {
+                    return (
+                      <IonCard
+                        className="m-0 mb-2 pb-2 card-slide w-100 afiliados"
+                        key={item.id}
+                      >
+                        <IonCardContent
+                          className="card-content-slide"
+                          id={`employeeItem_${item.id}`}
+                          key={item.id}
+                        >
+                          <IonImg
+                            src={`https://toolkit.maxialatam.com/wallethealth/asset/css.jpeg`}
+                            alt="employee avatar"
+                            style={{ width: "50px" }}
+                            className="float-left mr-2"
+                          />
+                          <div className="fs-15 font-w600 text-info mt-1 title">
+                            <span className="w-100">{item.nombre}</span>
+                            <span>
+                              <FontAwesomeIcon
+                                icon={faHeart}
+                                className="mr-0 float-right"
+                              />
+                            </span>
+                          </div>
+                          <div>
+                            <p className="mb-0 mt-1 fs-12 d-flex">
+                              <span>
+                                {item.descripcion}Cll 13a #76-52 - Piso 1
+                              </span>
+                            </p>
+                          </div>
+                        </IonCardContent>
+                      </IonCard>
+                    );
+                  })
+                : null}
+            </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
